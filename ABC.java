55dcf60dcea65882b4403cfdc894b706ed48557d
Ramya <rramya87499@gmail.com>
2022-10-27 11:19:57 +0530

    initial commit

---
+++ ABC.java
@@ -0,0 +1,30 @@
+public class ABC{
+
+int age=18;
+
+public int getAge(){
+
+System.out.println(age);
+return age;
+
+}
+void checkAge(int age)
+{
+if(age<18){
+System.out.println("not valid");
+
+}
+else
+{
+System.out.println("valid");
+}
+}
+public static void main(String[] args){
+
+ABC obj=new ABC();
+int i = obj.getAge();
+obj.checkAge(i);
+
+}
+}
+
